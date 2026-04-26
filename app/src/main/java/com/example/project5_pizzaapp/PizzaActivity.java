91033e228f4cbff6a7c4ace4130483697f4ef680
Ishani180 <iar30@scarletmail.rutgers.edu>
2026-04-26 18:13:34 -0400

    activity

--- app/src/main/java/com/example/project5_pizzaapp/PizzaActivity.java
+++ app/src/main/java/com/example/project5_pizzaapp/PizzaActivity.java
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+//comment
+
 /**
  * Displays available pizzas using RecyclerView.
  * Allows user to add pizzas to the current order.
